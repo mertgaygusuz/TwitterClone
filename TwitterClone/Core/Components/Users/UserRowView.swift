5788f271403cb0488f7d090372311eb86cedc424
mert gaygusuz <mertgaygusuz@hotmail.com>
2022-12-19 17:22:36 +0300

    view of explore uı created

---
+++ TwitterClone/Core/Components/Users/UserRowView.swift
@@ -0,0 +1,36 @@
+//
+//  UserRowView.swift
+//  TwitterClone
+//
+//  Created by Mert Gaygusuz on 19.12.2022.
+//
+
+import SwiftUI
+
+struct UserRowView: View {
+    var body: some View {
+        HStack(spacing: 12) {
+            Circle()
+                .frame(width: 48, height: 48)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("mertgaygusuz")
+                    .font(.subheadline).bold()
+                
+                Text("Mert Gaygusuz")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
+    }
+}
+
+struct UserRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserRowView()
+    }
+}
---
+++ TwitterClone/Core/SideMenu/Views/SideMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuView.swift
+//  TwitterClone
+//
+//  Created by Mert Gaygusuz on 19.12.2022.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
