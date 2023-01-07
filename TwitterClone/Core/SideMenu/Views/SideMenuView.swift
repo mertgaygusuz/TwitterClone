2cf3c0d479011220b98d50935e67238346dcf6bb
mert gaygusuz <mertgaygusuz@hotmail.com>
2023-01-07 21:23:51 +0300

    Kingfisher package is installed.

--- TwitterClone/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterClone/Core/SideMenu/Views/SideMenuView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct SideMenuView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
@@ -14,7 +15,10 @@ struct SideMenuView: View {
         if let user = authViewModel.currentUser {
             VStack(alignment: .leading, spacing: 32) {
                 VStack(alignment: .leading) {
-                    Circle()
+                    KFImage(URL(string: user.profileImageUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(Circle())
                         .frame(width: 48, height: 48)
                     
                     VStack(alignment: .leading, spacing: 4) {
